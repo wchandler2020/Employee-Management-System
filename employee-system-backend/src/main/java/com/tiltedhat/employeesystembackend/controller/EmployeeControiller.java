5ae20d24b559766426a00a5a5a9859a564accaa9
wchandler2020 <you@example.com>
2022-02-10 08:41:37 -0600

    added update and find by id actions to backend api

--- employee-system-backend/src/main/java/com/tiltedhat/employeesystembackend/controller/EmployeeControiller.java
+++ employee-system-backend/src/main/java/com/tiltedhat/employeesystembackend/controller/EmployeeControiller.java
@@ -39,4 +39,17 @@ public class EmployeeControiller {
         response.put("deleted", deleted);
         return ResponseEntity.ok(response);
     }
+
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
+        Employee employee = null;
+        employee = employeeService.getEmployeeById(id);
+        return ResponseEntity.ok(employee);
+    }
+
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employee){
+        employee = employeeService.updateEmployee(id, employee);
+        return ResponseEntity.ok(employee);
+    }
 }
